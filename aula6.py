22539619cc2994b642c5214c576cb185385e0cc0
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-12 14:25:46 -0300

    Update aula6.py

--- aula6.py
+++ aula6.py
@@ -1,3 +1,6 @@
-print('a' + 'b')
-print(11 + 4)
-print('aa' + 15)
+# Typecasting.
+print('a' + 'a')  # O sinal de mais quando usado em string, concatena uma na outra.
+print(10.5 + 4.5)  # O sinal de mais em números, sejam inteiros ou flutuantes, soma um valor com o outro.
+print('10.5' + 4.5)  # Como python é uma linguagem de tipagem forte, ele não pode realizar essa ação. Pra isso, deve ser feito a coerção de tipo.
+
+print(int('10,5') + float('4.5'))  # Usando as classes `int()`, `float()`, `str()`, `bool()` posso fazer a coerção dos dados.
