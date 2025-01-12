0468e527767e8e883bdb50cb55dd8a8b88846fd7
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-12 00:59:41 -0300

    anotações aula5

---
+++ aula5.py
@@ -0,0 +1,13 @@
+# Tipo de de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# Sim (True) ou não (False).
+# Existem varios operadores para "questionar".
+# dentre eles o ==, que um operador lógico que
+# questiona se um valor é igual a outro. -><- <> 
+
+print(10 == 10)  # Sim -> True (verdadeiro).
+print(10 != 10)  # Não -> False (falso).
+print(10 >=15)  # Sim -> True (verdadeiro).
+print(10 <= 15)  # Não -> False (falso).
+print(type(1 < 2))  # Boolean
