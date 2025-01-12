6aeae7ae87ba33526068e921c8ac39dc05eea203
Guilherme HF Port <guilhermehfport@gmail.com>
2025-01-12 00:42:44 -0300

    aula4 anotações

---
+++ aula4.py
@@ -0,0 +1,8 @@
+print(11)
+print(-11)   # Três valores inteiros
+print(0)
+print(11.5)
+print(-11.5)  # Três valores float
+print(0.0)
+print(type(12.07))  # Usando a classe type() para retornar o tipo do valor passado.
+print(type(2024))
